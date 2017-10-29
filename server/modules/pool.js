5bf8d5498f198d2bde1578809d57dd0678d4c1df
Sam Fortin <sam.j.fortin@gmail.com>
2017-10-29 10:02:10 -0500

    Pool connection set to deploy to heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,11 +1,32 @@
 var pg = require('pg');
+var url = require('url');
+var config = {};
 
-var config = {
-    database: 'sezzle',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-};
+if (process.env.DATABASE_URL) {
+    var params = url.parse(process.env.DATABASE_URL);
+    var auth = params.auth.split(':');
 
-module.exports = pg.Pool(config);
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+
+} else {
+    config = {
+        user: process.env.PG_USER || null,
+        password: process.env.DATABASE_SECRET || null,
+        host: process.env.DATABASE_SERVER || 'localhost',
+        port: process.env.DATABASE_PORT || 5432,
+        database: process.env.DATABASE_NAME || 'sezzle',
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+}
+
+module.exports = new pg.Pool(config);
